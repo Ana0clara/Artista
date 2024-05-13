33d8974ac369c4c13e54435e6f412284579013d0
Ana clara Barbosa <169068132+Ana0clara@users.noreply.github.com>
2024-05-13 17:04:32 -0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,45 @@
-
-
 public class Main {
   public static void main(String[] args) {
-    System.out.println("———");
-    System.out.println(" a ");
-    System.out.println("———");
-      
-    System.out.println("Album — ");
-    System.out.println("Ano de lançamento — ");
+    System.out.println("         ———————");
+    System.out.println("           Jao ");
+    System.out.println("         ———————");
+// nome do artista ou banda
+    System.out.println(" "); 
+    System.out.println(" "); 
+    System.out.println("----------------------- "); 
 
+
+    System.out.println("Album — Lobos ");
+    System.out.println("------------------------"); 
+
+    System.out.println("Ano de lançamento — 2018 ");
+    System.out.println("------------------------"); 
+// ano de lançamento do album
     System.out.println(" ");
   
-    System.out.println("*escrita*");
+    System.out.println("*Lobos é o álbum de estúdio de estreia do cantor brasileiro Jão, lançado em 17 de agosto de 2018, através da Head Media e Universal Music. Os temas líricos do álbum giram em torno de suas narrativas pessoais que deixam visíveis suas fragilidades, tristezas e medos*");
+    // sobre o album desse artista
+    System.out.println(" "); 
     System.out.println(" "); 
 
-    System.out.println("MUSICAS");
 
-    System.out.println("+  Titulo         duraçao"); 
-    System.out.println("1  nome aleatorio a       "); 
-    System.out.println("2"); 
+    System.out.println("          MUSICAS");
     System.out.println(" "); 
+
+
+    System.out.println("N  Titulo                duraçao"); 
     System.out.println(" "); 
+// trocar esse n por algo melhor
+    System.out.println("1  Vou morrer sozinho     02:55 "); 
+    System.out.println("2  ainda te amo           03:17 "); 
+    System.out.println("3  Lindo demais           03:01 "); 
+    System.out.println("4  Imaturo                02:59 "); 
+    System.out.println("5  Ainda te amo           03:11 "); 
+    System.out.println("6  A Rua                  02:37 "); 
+    System.out.println("7  Lobos                  03:44 "); 
+    System.out.println("8  Quero ser como voce    03:10 "); 
+    System.out.println("9  aqui                   02:52 "); 
+    System.out.println("10 Montros                04:16 "); 
 
     
   }
