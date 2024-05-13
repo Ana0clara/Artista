3d7a1bcabdc427f3a9f4701f9fcd542c70332429
Ana clara Barbosa <169068132+Ana0clara@users.noreply.github.com>
2024-05-13 14:37:41 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("———");
+    System.out.println(" a ");
+    System.out.println("———");
+      
+    System.out.println("Album — ");
+    System.out.println("Ano de lançamento — ");
+
+    System.out.println(" ");
+  
+    System.out.println("*escrita*");
+    System.out.println(" "); 
+
+    System.out.println("MUSICAS");
+
+    System.out.println("+  Titulo         duraçao"); 
+    System.out.println("1  nome aleatorio a       "); 
+    System.out.println("2"); 
+    System.out.println(" "); 
+    System.out.println(" "); 
+
+    
+  }
+
+}
